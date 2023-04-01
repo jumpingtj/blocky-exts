4ad9b16fae1ab3dcbdfc84bccdb921a192062d10
toby <toby@swinson-howell.net>
2023-04-01 11:51:09 +0100

    added movement

---
+++ exts/movement.js
@@ -0,0 +1,41 @@
+var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+
+class Movement {
+    constructor(runtime) {
+        this.runtime = runtime;
+        this.automove = false;
+    }
+    getInfo() {
+        return {
+            "id": "Movement",
+            "name": "Self-move",
+            "blocks": [{
+                "opcode": "movbool",
+                "blockType": "boolean",
+                "text": "Auto-move on?",
+                "arguments": {}
+            }, {
+                "opcode": "movset",
+                "blockType": "command",
+                "text": "Set Auto-move to [B]",
+                "arguments": {
+                    "B": {
+                        "type": "boolean",
+                        "defaultValue": "true"
+                    }
+                }
+            }]
+        }
+    }
+    movbool() {
+        return this.automove
+    }
+    movset(b) {
+        this.automove = b
+    }
+}
+
+Scratch.extensions.register(new Movement())
